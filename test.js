b87799ea38d633a1663b5f423acdc6aed99d2eb5
Mu-An Chiou <me@muanchiou.com>
2014-10-12 15:17:32 +0800

    Delete things, add things

--- test.js
+++
@@ -1,7 +0,0 @@
-var test = require('tape')
-test('loads a cat', function(t) {
-  t.notOk(!!document.querySelector('.cat-picture'))
-  require('./')
-  t.ok(!!document.querySelector('.cat-picture'))
-  t.end()
-})
